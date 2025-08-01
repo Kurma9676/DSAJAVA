d09c4758f9b7ef3f2563290148048f04cf38432c
SURNI KURMA RAO <kurmaraosurni@gmail.com>
2025-08-01 17:22:32 +0530

    Create validpalindrome.java

---
+++ validpalindrome.java
@@ -0,0 +1,41 @@
+/*A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.
+
+ 
+
+Example 1:
+
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+Example 2:
+
+Input: s = "race a car"
+Output: false
+Explanation: "raceacar" is not a palindrome.
+Example 3:
+
+Input: s = " "
+Output: true
+Explanation: s is an empty string "" after removing non-alphanumeric characters.
+Since an empty string reads the same forward and backward, it is a palindrome.*/
+import java.util.*;
+
+class Solution {
+    public boolean isPalindrome(String s) {
+        StringBuilder m = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (Character.isLetterOrDigit(ch)) {  // better to include digits too
+                m.append(Character.toLowerCase(ch)); // convert here itself
+            }
+        }
+
+        String original = m.toString();
+        String reversed = new StringBuilder(original).reverse().toString();
+
+        return original.equals(reversed);
+    }
+}

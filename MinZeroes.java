78baf41054deb506013912a1eb467bc3d8895e77
SURNI KURMA RAO <kurmaraosurni@gmail.com>
2025-09-14 23:19:22 +0530

    Create MinZeroes.java

---
+++ MinZeroes.java
@@ -0,0 +1,34 @@
+// 2357. Make Array Zero by Subtracting Equal Amounts
+// Solved
+// Easy
+// Topics
+// premium lock icon
+// Companies
+// Hint
+// You are given a non-negative integer array nums. In one operation, you must:
+
+// Choose a positive integer x such that x is less than or equal to the smallest non-zero element in nums.
+// Subtract x from every positive element in nums.
+// Return the minimum number of operations to make every element in nums equal to 0.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,5,0,3,5]
+// Output: 3
+// Explanation:
+// In the first operation, choose x = 1. Now, nums = [0,4,0,2,4].
+// In the second operation, choose x = 2. Now, nums = [0,2,0,0,2].
+// In the third operation, choose x = 2. Now, nums = [0,0,0,0,0].
+class Solution {
+    public int minimumOperations(int[] nums) {
+        Set<Integer> s=new HashSet<>();
+        for(int i:nums){
+            if(i>0){
+                s.add(i);
+            }
+        }
+        return s.size();
+    }
+}
